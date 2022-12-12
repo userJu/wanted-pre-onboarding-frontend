8ccdd7c86bc46efc4d342de932ae55c79484f6f5
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-12-12 22:14:42 +0900

    Feat: Input component 제작

---
+++ src/components/Input.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface inputProps {
+  inputId: string;
+  labelName: string;
+  placeholder: string;
+  handleChange: (e: React.FormEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  inputId,
+  labelName,
+  placeholder,
+  handleChange,
+}: inputProps) => {
+  return (
+    <div>
+      <label htmlFor="inputId">{labelName}</label>
+      <input
+        type="text"
+        id={inputId}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
+
+export default Input;
