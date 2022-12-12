ca534afdf53fc374b7b98eb706fc7837640a39cd
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-12-12 22:16:31 +0900

    Feat: Auth 관련 페이지에서 사용할 form 컴포넌트 제작
    비슷한 형식의 form을 재사용할 일이 login과 signup밖에 없는 것 같아
    pages 안에 components를 만들어 제작했다

---
+++ src/pages/auth/components/Form.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Input from "../../../components/Input";
+
+const Form = () => {
+  const handleEmailChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e);
+  };
+  const handlePasswordChange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e);
+  };
+
+  const emailInputProps = {
+    inputId: "email",
+    labelName: "email",
+    placeholder: "이메일을 입력해주세요",
+    handleChange: handleEmailChange,
+  };
+  const passwordInputProps = {
+    inputId: "password",
+    labelName: "password",
+    placeholder: "비밀번호를 입력해주세요",
+    handleChange: handlePasswordChange,
+  };
+
+  const onLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onLoginSubmit}>
+      <Input {...emailInputProps} />
+      <Input {...passwordInputProps} />
+      <button>제출하기</button>
+    </form>
+  );
+};
+
+export default Form;
