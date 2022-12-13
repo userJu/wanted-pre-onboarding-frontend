8e18e772d08b8b07e1e0a0767231f569cc6127e3
Juhyun <boochickenood@gmail.com>
2022-12-13 16:43:56 +0900

    Feat: Todo에서 이용할 Form 컴포넌트 제작
    Form으로 제작된 todo로 new Todo를 만드는 함수 생성

--- src/pages/todo/components/Form.tsx
+++ src/pages/todo/components/Form.tsx
@@ -1,21 +1,46 @@
 import React from "react";
 import Input from "../../../components/Input";
+import useInput from "../../../hooks/useInput";
+import { TodoType } from "../types";
 
-const Form = () => {
-  const handleTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-  };
+const Form = ({
+  setNewTodos,
+}: {
+  setNewTodos: (newTodo: TodoType) => void;
+}) => {
+  const todoInput = useInput("");
+
+  const { value, onChange, resetValue } = todoInput;
 
   const todoInputProps = {
     inputId: "todo",
     labelName: "todo",
     placeholder: "할 일을 입력해주세요",
-    handleChange: handleTodoChange,
+    value,
+    handleChange: onChange,
   };
 
   const onTodoSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.dir(e.target);
+    const newTodo = makeInitialTodo(value);
+    setNewTodos(newTodo);
+    resetValue();
+  };
+
+  const makeInitialTodo = (value: string) => {
+    // class방식으로 새로운 todo를 만들면 어떨까?
+
+    const date = new Date();
+
+    const newTodo = {
+      title: value,
+      content: "",
+      isCompleted: false,
+      createdAt: date,
+      // new Date, Date now blog 정리하기
+      key: +date,
+    };
+    return newTodo;
   };
 
   return (
