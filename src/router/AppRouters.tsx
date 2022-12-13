84612ee17c56b8d17f36172381de483efcd5e8b2
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-12-13 12:06:03 +0900

    Feat: Add todo 경로

--- src/router/AppRouters.tsx
+++ src/router/AppRouters.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import AuthPage from "../pages/auth/AuthPage";
+import TodoPage from "../pages/todo/TodoPage";
 
 const AppRouters = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
+      <Route path="/todo" element={<TodoPage />} />
     </Routes>
   );
 };
