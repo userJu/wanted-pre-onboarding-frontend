bbab1a5396648d60ed44e31d771d9300ff4279a0
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-12-13 12:07:22 +0900

    Feat: Todo page 기본 형태 제작

--- src/pages/todo/TodoPage.tsx
+++ src/pages/todo/TodoPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Form from "./components/Form";
 
 const Todo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Form></Form>
+    </div>
+  );
 };
 
 export default Todo;
