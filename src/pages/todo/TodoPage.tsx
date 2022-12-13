1619e5409e37cda63766299ce503ea06ca9e59bf
Juhyun <boochickenood@gmail.com>
2022-12-13 16:44:33 +0900

    Feat: Todo 페이지를 제작하고 List를 보여주기 위해 TodoList 제작

--- src/pages/todo/TodoPage.tsx
+++ src/pages/todo/TodoPage.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import Todos from "./components/Todos";
+import { TodoType } from "./types";
 
 const Todo = () => {
+  const [todos, setTodos] = useState<TodoType[] | []>([]);
+  const [todo, setTodo] = useState<TodoType | undefined>(undefined);
+  // const [selectedTodo, setSelectedTodo] = useState<number | null>(null);
+
+  useEffect(() => {
+    setNewestTodo();
+    console.log(todo);
+  }, [todos]);
+
+  const setNewestTodo = useCallback(() => {
+    const todoLength = todos.length;
+    setTodo(todos[todoLength - 1]);
+  }, [todos]);
+
+  const setNewTodos = (newTodo: TodoType) => {
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
+  const onTodoClick = (e: React.MouseEvent<HTMLLIElement>) => {
+    if (e.currentTarget.dataset.id) {
+      setSelectedTodo(parseInt(e.currentTarget.dataset.id));
+    }
+  };
+
+  const setSelectedTodo = (todoKey: number | undefined) => {
+    const selectedTodo = todos.find((todo) => todo.key === todoKey);
+    selectedTodo && setTodo(selectedTodo);
+  };
+
   return (
     <div>
-      <Form></Form>
+      <Form setNewTodos={setNewTodos}></Form>
+      <Todos></Todos>
+      {todos.map((todo) => (
+        <li onClick={onTodoClick} key={todo.key} data-id={todo.key}>
+          {todo.title}
+        </li>
+      ))}
+      <TodoList todo={todo}></TodoList>
     </div>
   );
 };
--- src/pages/todo/components/TodoList.tsx
+++ src/pages/todo/components/TodoList.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { TodoType } from "../types";
 
-const TodoLists = () => {
-  return <div></div>;
+const TodoLists = ({ todo }: { todo: TodoType | undefined }) => {
+  console.log(todo);
+  return (
+    <div>
+      {todo ? (
+        <div>
+          <h1>리스트</h1>
+          <h2>{todo.title}</h2>
+        </div>
+      ) : (
+        <div>
+          <h1>리스트</h1>
+          <h2>todo가 없어요 ㅜㅜ</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TodoLists;
